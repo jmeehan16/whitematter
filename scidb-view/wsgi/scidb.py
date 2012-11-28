92ade9506f4cc03442f1b39d6779ebecdee8d353
bfichter <benjamin_fichter@brown.edu>
2012-11-27 23:35:00 -0500

    debugging queryFrontTile

--- scidb-view/wsgi/scidb.py
+++ scidb-view/wsgi/scidb.py
@@ -197,7 +197,7 @@ def renderPngTop(width, height, rows):
             #<bfichter> to make it white, a little contrived but...
             #g.write(str(i) + "," + str(j) + "\n")
             #g.write(str(val) + "\n")
-            pix[width - j,height - i] = (val,val,val) #check these minuses
+            pix[j, i] = (val,val,val) #check these minuses
             j = (j+1)%height
             if j == 0:
                 i = (i+1)%width
