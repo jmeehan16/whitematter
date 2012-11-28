8597701e92af6fa68ce350db2cd05462b9510ea1
bfichter <benjamin_fichter@brown.edu>
2012-11-27 22:46:37 -0500

    debugging queryFrontTile

--- scidb-view/wsgi/scidb.py
+++ scidb-view/wsgi/scidb.py
@@ -114,7 +114,7 @@ def queryTopTile(brain,width,height,slicedepth):
     return renderPng2(width-1, height-1, rows)
 
 
-def queryFrontTile(brain, width, height, slicedepth):###switched width and height
+def queryFrontTile(brain, height, width, slicedepth):#switched width and height
     header, rows = querySciDB2("subarray(%s,%d,%d,%d,%d,%d,%d,%d,%d)" % (brain, slicedepth, 0, 0, 0, slicedepth, width - 1, height - 1, 0))#maybe swap width-1 and height-1
     return renderPng3(width-1, height-1, rows)
 
