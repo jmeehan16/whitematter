8a4f553917ed5a8361014f32767522205ff43ee6
bfichter <benjamin_fichter@brown.edu>
2012-12-01 18:51:12 -0500

    entire volume attempt

--- scidb-view/wsgi/scidb.py
+++ scidb-view/wsgi/scidb.py
@@ -183,7 +183,8 @@ def queryEntireVolume():
                 if y==0 and z==0:
                     x = (x+1) % width
                 counter+=1      
-                s.write("counter is  " + str(counter) + '\n') 
+                s.write("counter is  " + str(counter))
+                s.write("\n") 
             
     return volume
     
