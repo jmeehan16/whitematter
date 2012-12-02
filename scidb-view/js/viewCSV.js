9e0b4d9c60a8758e8604b9567555679b263cedcf
John <john@cs.brown.edu>
2012-12-01 20:27:39 -0500

    separating scidb from csv

---
+++ scidb-view/js/viewCSV.js
@@ -0,0 +1,277 @@
+$(function() {
+	console.log("started"); 
+	var dimensions = getJsonSync("/wm/wsgi/dimensions.wsgi?name=image");
+    initSliders();
+	//PanoJS.CREATE_THUMBNAIL_CONTROLS = false;
+	var viewers = new Array();
+	//var viewer2 = null;
+	//var viewer3 = null;
+	
+	var names = [];
+    var xhr=null;
+	function getJsonSync(url) {
+	    //don't let multiple ajax calls accumulate
+		if(xhr || xhr!=null) { 
+			xhr.abort(); 
+		}
+		else {
+			xhr = $.ajax({
+				type: "GET",
+				url: url,
+				dataType: "json",
+				success: function() {},
+				data: {},
+				async: false
+			});
+			return JSON.parse(xhr.responseText);
+		}
+	};
+
+	/*function zooms(name, names) {
+		var zoomNames = [];
+		for (i = 0; i < names.length; i++) {
+			var zoomName = name + "_zoom_" + i; 
+			if ($.inArray(zoomName, names) >= 0) {
+				zoomNames[i] = zoomName;
+			} else {
+				break;
+			}
+		}
+
+		if (zoomNames.length == 0)
+			zoomNames[0] = name;
+
+
+		return zoomNames;
+	};*/
+	
+	function initSliders() {
+	    var depth = dimensions["depth"];
+		//foreach viewer-container prepend a slider with max depth acquired
+		var viewers = $(".viewer-container")
+		viewers.each(function(i){ 
+						$(this).prepend('<input type="text" data-slider="true" id="slider'+i+'" data-slider-step="1" data-slider-theme="volume" data-slider-range="0,'+(depth-1)+'">');  
+					});
+		
+    }
+    
+	function wholebrain(brain,viewerid,viewtype){
+		var slicedepth = $("#"+viewerid).parent().find("input").val();
+		var width = dimensions["width"];
+		var height = dimensions["height"];
+		xhr = $.post("/wm/wsgi/multipleslices.wsgi",
+					{"brain": brain,
+					 "width": width,
+					 "height": height,
+					 "slicedepth": slicedepth,
+					 "viewtype": viewtype
+					},
+						function(data){ 
+						    console.log(data);
+							//do something with all the slices
+							//$(data).find("something").each(function() {)
+							//$('#'+viewerid+' .'+viewtype).append('<span class="slice" id="'+viewerid+'-'+brain+'-'+viewtype+'-'+slicedepth+'"><img src="data:image/png;base64,'+data+'"/></span>'); 
+							//$('#'+viewerid+' .'+viewtype+' .slice').hide().removeClass("visible");
+							//$('#'+viewerid+'-'+brain+'-'+viewtype+'-'+slicedepth).show().addClass("visible").show();
+						}
+		);
+	}
+	
+	
+	
+	function update(brain,viewerid,viewtype){ 
+		//var brain = brainlist.val(); //selected brain
+		//var viewerid = viewerslist.val(); //selected viewer
+		var width = dimensions["width"];
+		var height = dimensions["height"];
+		var slicedepth = $("#"+viewerid).parent().find("input").val();
+		//brain = "image"; //TODO REMOVE
+		if (slicedepth == null || !slicedepth)
+		    slicedepth = 120;
+		//check if this slice is there already 
+		if ($('#'+viewerid+'-'+brain+'-'+viewtype+'-'+slicedepth).length==0){
+			//if(xhr || xhr!=null) { 
+			//	xhr.abort(); 
+			//}
+			//else {
+				xhr = $.post("/wm/wsgi/slice.wsgi",
+					{"brain": brain,
+					 "width": width,
+					 "height": height,
+					 "slicedepth": slicedepth,
+					 "viewtype": viewtype
+					},
+					function(data){ 
+						$('#'+viewerid+' .'+viewtype).append('<span class="slice" id="'+viewerid+'-'+brain+'-'+viewtype+'-'+slicedepth+'"><img src="data:image/png;base64,'+data+'"/></span>'); 
+						$('#'+viewerid+' .'+viewtype+' .slice').hide().removeClass("visible");
+						$('#'+viewerid+'-'+brain+'-'+viewtype+'-'+slicedepth).show().addClass("visible").show();
+					}
+				);
+			//}
+		}
+		else {
+			$('#'+viewerid+' .'+viewtype+' .slice').hide().removeClass("visible");
+			$('#'+viewerid+'-'+brain+'-'+viewtype+'-'+slicedepth).show().addClass("visible").show();
+		}
+	}	
+
+	/*function show(brainlist,viewerslist) {
+		var brain = brainlist.val(); //selected brain
+		var viewerid = viewerslist.val(); //selected viewer
+		console.log("detecting zooms"); 
+		zoomNames = zooms(brain, names);
+		zoomMax = zoomNames.length - 1;
+		console.log("detected " + zoomNames.length + " zooms"); 
+
+		console.log("detecting dimensions"); 
+		var width = dimensions["width"];
+		var height = dimensions["height"];
+		
+		var tileSize = 256;
+		console.log("detected width = " + width); 
+		console.log("detected height = " + height); 
+		console.log("detected tileSize = " + tileSize); 
+
+		console.log("started viewer");
+		var provider = new PanoJS.TileUrlProvider('','','');
+		provider.assembleUrl = function(x, y, zoom) {
+			var zoomIdx = zoom > zoomMax ? zoomMax: zoom;
+			url = "/wm/wsgi/slice.wsgi?name=" + zoomNames[zoomIdx];
+			url += "&width=" + tileSize;
+			url += "&height=" + tileSize;
+			url += "&x=" + x;
+			url += "&y=" + y;
+			url += "&z=" + "120"
+			return url;
+		}
+		if (viewers[viewerid])
+			viewers[viewerid].clear();
+
+		viewers[viewerid] = new PanoJS(viewerid, {
+			tileUrlProvider : provider,
+			tileSize        : tileSize,
+			maxZoom         : zoomNames.length - 1,
+			imageWidth      : width,
+			imageHeight     : height,
+			blankTile       : "images/blank.gif",
+			loadingTile     : "images/progress.gif"
+		});
+
+		Ext.EventManager.addListener(window, "resize", callback(viewers[viewerid], viewers[viewerid].resize));
+		xhr = "something"
+		viewers[viewerid].init();
+		xhr = null
+		console.log("built viewer");
+	};*/
+	
+	/*function update(viewerid) {
+		var width = dimensions["width"];
+		var height = dimensions["height"];
+		
+		var tileSize = 256;
+		var provider = new PanoJS.TileUrlProvider('','','');
+		var z = $("#"+viewerid).parent().find("input").val()
+		provider.assembleUrl = function(x, y, zoom) {
+			var zoomIdx = zoom > zoomMax ? zoomMax: zoom;
+			url = "/wm/wsgi/slice.wsgi?name=image_50chunk";
+			url += "&width=" + tileSize;
+			url += "&height=" + tileSize;
+			url += "&x=" + x;
+			url += "&y=" + y;
+			url += "&z=" + z;
+			return url;
+		}
+                if (viewers[viewerid])
+			viewers[viewerid].clear();
+                 
+		viewers[viewerid] = new PanoJS(viewerid, {
+			tileUrlProvider : provider,
+			tileSize        : tileSize,
+			maxZoom         : zoomNames.length - 1,
+			imageWidth      : width,
+			imageHeight     : height,
+			blankTile       : "images/blank.gif",
+			loadingTile     : "images/progress.gif"
+		});
+
+		//Ext.EventManager.addListener(window, "resize", callback(viewers[viewerid], viewers[viewerid].resize));
+		xhr = "something"
+		viewers[viewerid].init();
+		console.log("updated viewer");
+		xhr = null
+	
+	}*/
+	
+	
+	function populateListOfBrains(names, sel) {
+		var nameSelection = $(sel);
+		var nameOptions = nameSelection.prop("options");
+		$("options", nameSelection).remove();
+		names.forEach(function(name) {
+			nameOptions[nameOptions.length] = new Option(name, name);
+		});
+		nameSelection.val(names[0]);
+	}
+	
+	function populateListOfViewers(){
+		var viewers = $(".viewer");
+		var ViewersSelection = $("#viewers");
+		viewers.each(function() { 
+						var id = $(this).attr("id");
+						ViewersSelection.append('<option value="'+id+'">'+id+'</option>');
+		            })
+    }					
+	  
+	/*Ext.onReady(function() {
+		console.log("initializing"); 
+		names = getJsonSync("/wm/wsgi/list.wsgi").names;
+		console.log(names);
+		populateListOfViewers();
+		populateListOfBrains(names, "#brains");
+		$("#choose .submitbutton").click(function() { show($(this).parent().find("#brains"),$(this).parent().find("#viewers"));})
+		//document.show.submit.onclick = update;
+		console.log("initialized"); 
+	});*/
+	
+	
+	
+	$(document).ready(function() {
+			console.log("jquery proper start");
+			names = getJsonSync("/wm/wsgi/list.wsgi").names;
+			console.log(names);
+			populateListOfViewers();
+			populateListOfBrains(names, "#brains");
+			$("#choose .submitbutton").click(function() {
+                    var viewerselected = $("#viewers").val();			
+			        if (viewerselected != "viewer2"){
+						update( $("#brains").val(), $("#viewers").val(),"top");
+						update( $("#brains").val(), $("#viewers").val(),"front");
+						update( $("#brains").val(), $("#viewers").val(),"side");
+					}
+					else {
+						wholebrain($("#brains").val(), $("#viewers").val(),"top");
+						//wholebrain($("#brains").val(), $("#viewers").val(),"front");
+						//wholebrain($("#brains").val(), $("#viewers").val(),"side");
+					}
+				}
+			)
+			$("[data-slider]").each(function () {
+									var input = $(this);
+									$("<span>").addClass("output").insertAfter($(this));
+								}).bind("slider:ready slider:changed", function (event, data) {
+																			$(this).nextAll(".output:first").html(data.value);
+																			var vieweridchanged=$(this).parent().find(".viewer").attr("id");
+																			
+																			//if(xhr || xhr!=null) { 
+																			//	//xhr.abort(); 
+																			//	xhr = null;
+																			//}
+																			//else {
+																				update($("#brains").val(),vieweridchanged,"top");
+																				update($("#brains").val(),vieweridchanged,"front");
+																				update($("#brains").val(),vieweridchanged,"side");
+																			//}
+																	   });
+	});
+
+});
--- scidb-view/wsgi/scidb.py
+++ scidb-view/wsgi/scidb.py
@@ -12,6 +12,9 @@ import datetime
 import base64
 import csv
 
+sys.path.append('/var/www/wm/wsgi')
+import render
+
 volume = dict()
 width = 0
 height = 0
@@ -88,7 +91,7 @@ def queryImage(name):
     width, height = queryDimensions(name)
     header, rows = querySciDB("scan(%s)" % name)
 
-    return renderPng(width, height, rows)
+    return render.renderPng(width, height, rows)
 
 #def queryTopTile(name, width, height, x, y, z):
 #    """Render an image of a tile of the specified array, returning a string
@@ -119,74 +122,25 @@ def queryImage(name):
  	this is because the orientations were not 'consistent' in scidb so in order to keep the three views oriented correctly 
 	relative to each other (eyes/neck pointed same way) the semantics of width and height are broken"""
 def queryTopTile(brain,width1,height1,slicedepth):
-    #header, rows = querySciDB2("subarray(%s,%d,%d,%d,%d,%d,%d,%d,%d)" % (brain, 0, 0, slicedepth, 0, width - 1, height - 1,slicedepth,0))
-    #return renderPngTop(width-1, height-1, rows)
+    header, rows = querySciDB2("subarray(%s,%d,%d,%d,%d,%d,%d,%d,%d)" % (brain, 0, 0, slicedepth, 0, width - 1, height - 1,slicedepth,0))
+    return render.renderPngTop(width-1, height-1, rows)
     
-    volume = queryEntireVolume()
+    #volume = queryEntireVolume()
     #f = open("/var/log/scidbpy_log.txt", 'w+')
     #f.write("volume of 90, 100  " + str(volume[90,100, 90])) 
     #return renderPngTop2(slicedepth, volume)
-    return renderPngDummy()
+    #return renderPngDummy()
 
 def queryFrontTile(brain, height, width, slicedepth):
     header, rows = querySciDB2("subarray(%s,%d,%d,%d,%d,%d,%d,%d,%d)" % (brain, slicedepth, 0, 0, 0, slicedepth, width - 1, height - 1, 0))#maybe swap width-1 and height-1
-    return renderPngFrontSide(width-1, height-1, rows)
+    return render.renderPngFrontSide(width-1, height-1, rows)
     #return renderPngDummy()
 
 def querySideTile(brain, height, width, slicedepth):
     header, rows = querySciDB2("subarray(%s,%d,%d,%d,%d,%d,%d,%d,%d)" % (brain, 0, slicedepth, 0, 0, width-1, slicedepth, height - 1, 0))#maybe swap width-1 and height-1
-    return renderPngFrontSide(width-1, height-1, rows)
+    return render.renderPngFrontSide(width-1, height-1, rows)
     #return renderPngDummy()
-     
-
-def queryEntireVolume():
-    # open("001.csv")
-    # first line = width
-    # sec line = height
-    # third line = depth
-    # fourth line start with loop
-	# volume = {"x":"y":"z":"v"}
-	# volume = dict()
-    #
-    #lines = [line.rstrip('\n') for line in open('000.csv')] # this should be a list of the lines without new line character
-
-    global volume
-    global width
-    global heigth
-    global depth
-
-    if len(volume.keys())==0:
-    #if 1 == 1:
-        f = open('/opt/whitematter/data/csv/000.csv', 'r')
-#        s = open("/var/log/scidbdebug.txt", 'a')
-        
-        x = 0
-        y = 0
-        z = 0
-        counter = 0
-        
-        for line in f:
-            if counter ==0:
-                width = int(line)
-                counter = 1
-            elif counter ==1:
-                height = int(line)
-                counter = 2
-            elif counter ==2:
-                depth = int(line)
-                counter = 3
-            else:
-                volume[x,y,z] = int(line)
-                z = (z+1) % depth
-                if z == 0:
-                    y = (y+1) % height
-                if y==0 and z==0:
-                    x = (x+1) % width
-                #counter+=1      
-                #s.write("counter is  " + str(counter))
-                #s.write("\n") 
-            
-    return volume
+   
     
 def removeArrays(pattern):
     import re
