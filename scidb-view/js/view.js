82c75429d24163e27524fe7f6581992561b62123
Michail Michailidis <mmichaelid@gmail.com>
2012-12-02 04:11:14 -0500

    possible infinite recursive javascript call fix?

--- scidb-view/js/view.js
+++ scidb-view/js/view.js
@@ -73,13 +73,14 @@ $(function() {
 					$('#slice-input-'+i).val( sliderchanged.slider( "value" ) );
 					
 					//sync with other sliders
+					valh=sliderchanged.slider("value")
 					$(".slider").each(function(){
-						valh=sliderchanged.slider("value")
-						$(this).slider('value',valh)
-						hs=$(this).slider();
-						hs.slider('option', 'value',valh);
-						hs.slider('option','slide').call(hs,null,{ handle: $('.ui-slider-handle', hs), value: valh });
-						
+						if ($(this)!=sliderchanged.slider()){
+							$(this).slider('value',valh)
+							hs=$(this).slider();
+							hs.slider('option', 'value',valh);
+							hs.slider('option','slide').call(hs,null,{ handle: $('.ui-slider-handle', hs), value: valh });
+						}
 						//$(this).slider().trigger('slide',{ ui: $('.ui-slider-handle', $(this)), value: sliderchanged.slider("value") });
 						//$('#slice-input-'+i).val( sliderchanged.slider( "value" ) );
 					});
