1c09c91ece93573f788f810f945bde74361556cc
bfichter <benjamin_fichter@brown.edu>
2012-12-01 22:08:09 -0500

    getting csv ready

--- scidb-view/wsgi/csvwrapper.py
+++ scidb-view/wsgi/csvwrapper.py
@@ -48,33 +48,31 @@ def queryEntireVolume():
     heigth = 0
     depth = 0
 
-    #if len(volume.keys())==0:
-    if 1 == 1:
-        f = open('/opt/whitematter/data/csv/000.csv', 'r')
+    f = open('/opt/whitematter/data/csv/000.csv', 'r')
 #        s = open("/var/log/scidbdebug.txt", 'a')
         
-        x = 0
-        y = 0
-        z = 0
-        counter = 0
+    x = 0
+    y = 0
+    z = 0
+    counter = 0
         
-        for line in f:
-            if counter ==0:
-                width = int(line)###MAYBE DO line.rstrip('\n') for all the line usages
-                counter = 1
-            elif counter ==1:
-                height = int(line)
-                counter = 2
-            elif counter ==2:
-                depth = int(line)
-                counter = 3
-            else:
-                volume[x,y,z] = int(line)
-                z = (z+1) % depth
-                if z == 0:
-                    y = (y+1) % height
-                if y==0 and z==0:
-                    x = (x+1) % width
+    for line in f:
+        if counter ==0:
+            width = int(line)###MAYBE DO line.rstrip('\n') for all the line usages
+            counter = 1
+        elif counter ==1:
+            height = int(line)
+            counter = 2
+        elif counter ==2:
+            depth = int(line)
+            counter = 3
+        else:
+            volume[x,y,z] = int(line)
+            z = (z+1) % depth
+            if z == 0:
+                y = (y+1) % height
+            if y==0 and z==0:
+                x = (x+1) % width
                 #counter+=1      
                 #s.write("counter is  " + str(counter))
                 #s.write("\n") 
