47daa5c5f74e649928d00202e173177e6f16ef58
John Meehan <john@cs.brown.edu>
2012-12-01 19:20:30 -0500

    adding listCSV.py

---
+++ scidb-view/wsgi/listCSV.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+
+import os
+import json
+import sys
+import time
+import subprocess      
+import Image
+import StringIO
+import urlparse
+import datetime
+
+sys.path.append('/var/www/wm/wsgi')
+import scidb
+
+def application(environ, start_response):
+    f = open('/var/log/list_log.txt','w+')
+    f.write('time: ' + str(datetime.datetime.now()))
+    #names = scidb.queryList()
+	from os import listdir
+	from os.path import isfile, join
+	names = [ f for f in listdir("/opt/whitematter/data/csv") if isfile(join(mypath,f)) ]
+
+    content = {"names": names}
+    start_response('200 OK', [('Content-Type', 'image/json')])
+    
+    #f.write('\n')
+    #f.write(json.dumps({"names" : names}))
+
+    #path = os.path.dirname(scidb2.__file__)
+    #f.write("\npath: " + str(path))
+
+    return [json.dumps(content)]
+    #return [json.dumps({"names" : "john"})]
+    
+if __name__ == "__main__":
+    sys.stdout.write("started\n")
+    #f = open('/var/log/list_log.txt','w')
+
+    sys.stdout.write("querying list\n")
+    #names = scidb.queryList()
+	from os import listdir
+	from os.path import isfile, join
+	names = [ f for f in listdir("/opt/whitematter/data/csv") if isfile(join(mypath,f)) ]
+
+    sys.stdout.write("printing list\n")
+    print names
+
+    sys.stdout.write("finished\n")
