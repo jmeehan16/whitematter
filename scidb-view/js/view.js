7b5de024e38dc1a080ec6547136bbd2548293f8a
Michail Michailidis <mmichaelid@gmail.com>
2012-12-04 17:01:02 -0500

    starting building the color bars

--- scidb-view/js/view.js
+++ scidb-view/js/view.js
@@ -6,6 +6,7 @@ $(function() {
 	var timer1;
 	var timer2;
 	initSliders();
+	initColorBars();
 	//PanoJS.CREATE_THUMBNAIL_CONTROLS = false;
 	var viewers = new Array();
 	//var viewer2 = null;
@@ -30,6 +31,16 @@ $(function() {
 			return JSON.parse(xhr.responseText);
 		}
 	};
+	
+	
+	function initColorBars(){
+		$("div.top").each(function(){ $(this).append('<div class="horizontal topbar colorbar"></div>'); });
+		$("div.top").each(function(){ $(this).append('<div class="vertical topbar colorbar"></div>'); });
+		$("div.side").each(function(){ $(this).append('<div class="horizontal sidebar colorbar"></div>'); });
+		$("div.side").each(function(){ $(this).append('<div class="vertical sidebar colorbar"></div>'); });
+		$("div.front").each(function(){ $(this).append('<div class="horizontal frontbar colorbar"></div>'); });
+		$("div.front").each(function() { $(this).append('<div class="vertical frontbar colorbar"></div>'); });
+	}
 	
 	
 	function initSliders() {
@@ -493,93 +504,6 @@ $(function() {
 			$('#'+viewerid+'-'+brain+'-'+viewtype+'-'+slicedepth).show().addClass("visible").show();
 		}
 	}	
-
-	/*function show(brainlist,viewerslist) {
-		var brain = brainlist.val(); //selected brain
-		var viewerid = viewerslist.val(); //selected viewer
-		console.log("detecting zooms"); 
-		zoomNames = zooms(brain, names);
-		zoomMax = zoomNames.length - 1;
-		console.log("detected " + zoomNames.length + " zooms"); 
-
-		console.log("detecting dimensions"); 
-		var width = dimensions["width"];
-		var height = dimensions["height"];
-		
-		var tileSize = 256;
-		console.log("detected width = " + width); 
-		console.log("detected height = " + height); 
-		console.log("detected tileSize = " + tileSize); 
-
-		console.log("started viewer");
-		var provider = new PanoJS.TileUrlProvider('','','');
-		provider.assembleUrl = function(x, y, zoom) {
-			var zoomIdx = zoom > zoomMax ? zoomMax: zoom;
-			url = "/wm/wsgi/slice.wsgi?name=" + zoomNames[zoomIdx];
-			url += "&width=" + tileSize;
-			url += "&height=" + tileSize;
-			url += "&x=" + x;
-			url += "&y=" + y;
-			url += "&z=" + "120"
-			return url;
-		}
-		if (viewers[viewerid])
-			viewers[viewerid].clear();
-
-		viewers[viewerid] = new PanoJS(viewerid, {
-			tileUrlProvider : provider,
-			tileSize        : tileSize,
-			maxZoom         : zoomNames.length - 1,
-			imageWidth      : width,
-			imageHeight     : height,
-			blankTile       : "images/blank.gif",
-			loadingTile     : "images/progress.gif"
-		});
-
-		Ext.EventManager.addListener(window, "resize", callback(viewers[viewerid], viewers[viewerid].resize));
-		xhr = "something"
-		viewers[viewerid].init();
-		xhr = null
-		console.log("built viewer");
-	};*/
-	
-	/*function update(viewerid) {
-		var width = dimensions["width"];
-		var height = dimensions["height"];
-		
-		var tileSize = 256;
-		var provider = new PanoJS.TileUrlProvider('','','');
-		var z = $("#"+viewerid).parent().find("input").val()
-		provider.assembleUrl = function(x, y, zoom) {
-			var zoomIdx = zoom > zoomMax ? zoomMax: zoom;
-			url = "/wm/wsgi/slice.wsgi?name=image_50chunk";
-			url += "&width=" + tileSize;
-			url += "&height=" + tileSize;
-			url += "&x=" + x;
-			url += "&y=" + y;
-			url += "&z=" + z;
-			return url;
-		}
-                if (viewers[viewerid])
-			viewers[viewerid].clear();
-                 
-		viewers[viewerid] = new PanoJS(viewerid, {
-			tileUrlProvider : provider,
-			tileSize        : tileSize,
-			maxZoom         : zoomNames.length - 1,
-			imageWidth      : width,
-			imageHeight     : height,
-			blankTile       : "images/blank.gif",
-			loadingTile     : "images/progress.gif"
-		});
-
-		//Ext.EventManager.addListener(window, "resize", callback(viewers[viewerid], viewers[viewerid].resize));
-		xhr = "something"
-		viewers[viewerid].init();
-		console.log("updated viewer");
-		xhr = null
-	
-	}*/
 	
 	
 	function populateListOfBrains(names, sel) {
@@ -600,17 +524,7 @@ $(function() {
 						ViewersSelection.append('<option value="'+id+'">'+id+'</option>');
 		            })
     }					
-	  
-	/*Ext.onReady(function() {
-		console.log("initializing"); 
-		names = getJsonSync("/wm/wsgi/list.wsgi").names;
-		console.log(names);
-		populateListOfViewers();
-		populateListOfBrains(names, "#brains");
-		$("#choose .submitbutton").click(function() { show($(this).parent().find("#brains"),$(this).parent().find("#viewers"));})
-		//document.show.submit.onclick = update;
-		console.log("initialized"); 
-	});*/
+
 	
 	
 	
@@ -634,23 +548,6 @@ $(function() {
 					//}
 				}
 			)
-			/*$("[data-slider]").each(function () {
-									var input = $(this);
-									$("<span>").addClass("output").insertAfter($(this));
-								}).bind("slider:ready slider:changed", function (event, data) {
-																			$(this).nextAll(".output:first").html(data.value);
-																			var vieweridchanged=$(this).parent().find(".viewer").attr("id");
-																			
-																			//if(xhr || xhr!=null) { 
-																			//	//xhr.abort(); 
-																			//	xhr = null;
-																			//}
-																			//else {
-																				update($("#brains").val(),vieweridchanged,"top");
-																				update($("#brains").val(),vieweridchanged,"front");
-																				update($("#brains").val(),vieweridchanged,"side");
-																			//}
-																	   });*/
 	});
 
 });
