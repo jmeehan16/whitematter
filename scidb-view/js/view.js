7136d1827ef52daa3fa0b98b3ce29d2683ae222f
Michail Michailidis <mmichaelid@gmail.com>
2012-12-04 17:37:05 -0500

    putting color bars in slice container so they take the actual size of slices

--- scidb-view/js/view.js
+++ scidb-view/js/view.js
@@ -35,12 +35,12 @@ $(function() {
 	
 	
 	function initColorBars(){
-		$("div.top").each(function(){ $(this).append('<div class="horizontal sidebar colorbar"></div>'); });
-		$("div.top").each(function(){ $(this).append('<div class="vertical frontbar colorbar"></div>'); });
-		$("div.side").each(function(){ $(this).append('<div class="horizontal topbar colorbar"></div>'); });
-		$("div.side").each(function(){ $(this).append('<div class="vertical frontbar colorbar"></div>'); });
-		$("div.front").each(function(){ $(this).append('<div class="horizontal topbar colorbar"></div>'); });
-		$("div.front").each(function() { $(this).append('<div class="vertical sidebar colorbar"></div>'); });
+		$("div.top .slicecontainer").each(function(){ $(this).prepend('<div class="horizontal sidebar colorbar"></div>'); });
+		$("div.top .slicecontainer").each(function(){ $(this).prepend('<div class="vertical frontbar colorbar"></div>'); });
+		$("div.side .slicecontainer").each(function(){ $(this).prepend('<div class="horizontal topbar colorbar"></div>'); });
+		$("div.side .slicecontainer").each(function(){ $(this).prepend('<div class="vertical frontbar colorbar"></div>'); });
+		$("div.front .slicecontainer").each(function(){ $(this).prepend('<div class="horizontal topbar colorbar"></div>'); });
+		$("div.front .slicecontainer").each(function() { $(this).prepend('<div class="vertical sidebar colorbar"></div>'); });
 	}
 	
 	
