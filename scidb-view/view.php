023025f59a5dab5edd6857ebb8ff7807b3b56b13
Michail Michailidis <mmichaelid@gmail.com>
2012-12-05 16:08:06 -0500

    single php file for Mysql,Scidb and CSV

---
+++ scidb-view/view.php
@@ -0,0 +1,81 @@
+<?php
+if ($_GET["storage"]=="mysql"){
+	$bodyclass="mysql";
+	$titletext="MySQL";
+}
+else if($_GET["storage"]=="csv"){
+	$bodyclass="csv";
+	$titletext="CSV";
+}
+else {
+	$bodyclass="scidb";
+	$titletext="SciDB";
+}
+?>
+
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8"/>
+<title><?php echo $titletext;?> Coordinated Viewer</title>
+
+<style type="text/css">@import url(css/style.css);</style>
+<link rel="stylesheet" href="http://code.jquery.com/ui/1.9.2/themes/base/jquery-ui.css" />
+<script src="http://code.jquery.com/jquery-1.8.3.js"></script>
+<script src="http://code.jquery.com/ui/1.9.2/jquery-ui.js"></script>
+<script type="text/javascript" src="js/view.js"></script>
+                
+</head>
+<body class="<?php echo $bodyclass;?>">
+<h1><?php echo $titletext;?> Coordinated Viewer</h1>
+
+<div id="choose">
+	<form name="show">
+		<select id="studies"></select>
+		<select id="brains"></select>
+		<select id="viewers"></select>
+		<input type="button" name="submit" value="Show" class="submitbutton"></input>
+	</form>
+</div>
+
+<div class="viewer-container">
+    <div id="viewer" class="viewer" style="width: 100%; height: 100%;">
+		<div class="top">
+			<div class="slice-container"></div>
+		</div>
+		<div class="side">
+			<div class="slice-container"></div>
+		</div>
+		<div class="front">
+			<div class="slice-container"></div>
+		</div>
+	</div>
+</div>
+<div class="viewer-container"> 
+	<div id="viewer2" class="viewer" style="width: 100%; height: 100%;">
+		<div class="top">
+			<div class="slice-container"></div>
+		</div>
+		<div class="side">
+			<div class="slice-container"></div>
+		</div>
+		<div class="front">
+			<div class="slice-container"></div>
+		</div>
+	</div>
+</div>
+<div class="viewer-container"> 
+	<div id="viewer3" class="viewer" style="width: 100%; height: 100%;" >
+		<div class="top">
+			<div class="slice-container"></div>
+		</div>
+		<div class="side">
+			<div class="slice-container"></div>
+		</div>
+		<div class="front">
+			<div class="slice-container"></div>
+		</div>
+	</div>
+</div>
+</body>
+</html>
