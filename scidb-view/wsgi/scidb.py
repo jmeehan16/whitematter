aa2ae35829c3cda41580654b3c385315fe099df2
bfichter <benjamin_fichter@brown.edu>
2012-12-08 19:08:48 -0500

    trying to unscramble

--- scidb-view/wsgi/scidb.py
+++ scidb-view/wsgi/scidb.py
@@ -95,7 +95,6 @@ def queryImage(name):
 
 def queryTopTile(brain,width,height,slicedepth,volume):
     header, rows = querySciDB2("subarray(%s,%d,%d,%d,%d,%d,%d,%d,%d)" % (brain, 0, 0, slicedepth, volume, width - 1, height - 1,slicedepth,volume))
-    #header, rows = querySciDB2("subarray(%s,%d,%d,%d,%d,%d,%d,%d,%d)" % (brain, 0, 0, slicedepth, volume, height - 1, width - 1,slicedepth,volume))
     return render.renderPngTop(width-1, height-1, rows)
 
 def queryFrontTile(brain, width, depth, slicedepth,volume):
