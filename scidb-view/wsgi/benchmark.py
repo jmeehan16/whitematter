29344eb0e759053ba27ce50a87d4a88df64fe332
John <john@cs.brown.edu>
2012-12-11 00:52:59 -0500

    added benchmark

--- scidb-view/wsgi/benchmark.py
+++ scidb-view/wsgi/benchmark.py
@@ -20,14 +20,16 @@ def resetFile():
 def startTimer(process):
     f = open("/var/log/benchmark","a")
     f.write("starting " + str(process) + "\n")
-    starttime = time()
+    starttime = time.time()
     f.write("start: " + str(starttime) + "\n")
     return starttime
 
 def endTimer(process, starttime):
     f = open("/var/log/benchmark","a")
-    endtime = time()
+    endtime = time.time()
     f.write("ending " + str(process) + "\n")
     f.write("end: " + str(endtime) + "\n")
     f.write("duration: " + str(endtime - starttime) + "\n\n")
     return endtime
+
+
